a15ccaee02a47cc93b370a1cec0c1bed715d704f
Caryn <cmccarthy15@gmail.com>
2018-10-13 15:41:47 -0400

    Update data.js

--- data.js
+++ data.js
@@ -1,5 +1,22 @@
 // Example
-let message = "HackerMan is in";
+let message = "HackeMan is in";
 console.log(message);
 
+let doesLikeBacon = true;
+console.log(doesLikeBacon);
+
 // Type your code below!
+
+
+
+
+
+
+
+
+
+
+
+// console.log before you declare
+// declaring twice
+// variable names with spaces
