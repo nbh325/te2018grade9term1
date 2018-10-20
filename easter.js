179670c5988bc8a0d21d0eb83a1ca1a8f0a72066
Coach Caryn <cmccarthy15@gmail.com>
2018-10-20 16:44:05 -0400

    tester commit

--- easter.js
+++ easter.js
@@ -1,5 +1,6 @@
 // import readline-sync module, don't touch!
 const READLINE = require("readline-sync");
 
+const helpMe = "Help Me!"
 // Write your program below!
 // First line should get user input (year) and store it in variable y.
